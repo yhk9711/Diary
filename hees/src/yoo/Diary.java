3da2b3d0fd50b4123099c081fd5b318fc622e072
yhk97 <yhk97@DESKTOP-86E4H65>
2017-06-25 15:04:31 +0900

    hing

--- hees/src/yoo/Diary.java
+++ hees/src/yoo/Diary.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
  public class Diary {
 	 private double inbalance;
-	private double balance;
+	private static double balance;
 	protected static int count=0;
 	protected static int[] date;
 	protected static int[] cloth;
@@ -86,45 +86,60 @@ import java.util.Scanner;
 
 			System.out.println("내가 가지고 있는 금액: " + diary.getBalance() );
 			
-			while(count<100000){
-
-				int tday=0;
-				System.out.printf("날짜: ");
-				tday=scan.nextInt();
-				diary.date(tday);
+			int k=0;
+			
+			while(true){
+				System.out.println("1번 : 지출 입력"+"\n"+"2번 : 지출 내역"+"\n"+"3번 : 초기화"+"\n");
+				k=scan.nextInt();
 				
-				int tcloth=0;
-				System.out.println("옷에 쓴 돈 : " );
-				tcloth=scan.nextInt();
-				diary.cloth(tcloth);
-				 
-				int tfood=0;
-				System.out.println("음식에 쓴 돈 : ");
-				tfood=scan.nextInt();
-				diary.food(tfood);
 				
-				int tanni=0;
-				System.out.println("기념일에 쓴  돈 : ");
-				tanni=scan.nextInt();
-				diary.anniversary(tanni);
-				
-				count++;	
-				for(int j=0;j<count;j++){
-					   System.out.println("날짜 : "+date[j]+"\n"+"옷에 쓴 돈은 : "+cloth[j]+"\n"+"음식에 쓴 돈은 : "+food[j]+"\n"+"기념일에 쓴 돈은 : "+anniversary[j]+"\n");
+				if(k==1){
+					int tday=0;
+					System.out.printf("날짜: ");
+					tday=scan.nextInt();
+					diary.date(tday);
+					
+					int tcloth=0;
+					System.out.println("옷에 쓴 돈 : " );
+					tcloth=scan.nextInt();
+					diary.cloth(tcloth);
+					 
+					int tfood=0;
+					System.out.println("음식에 쓴 돈 : ");
+					tfood=scan.nextInt();
+					diary.food(tfood);
+					
+					int tanni=0;
+					System.out.println("기념일에 쓴  돈 : ");
+					tanni=scan.nextInt();
+					diary.anniversary(tanni);
+					
+					count++;
 				}
-					   
-				System.out.println("옷에 쓴 총 금액은:"+clothtotal+"\n"+"음식에 쓴 총 금액은:"+foodtotal+"\n"+"기념일에 쓴 총 금액은:"+annitotal+"\n");
-				System.out.println("총 금액 :"+diary.price());
-				System.out.printf("옷에 쓴 돈의 비율 :%.2f\n ",diary.clothper(clothtotal));
-				System.out.printf("음식에 쓴 돈의 비율 :%.2f\n ",diary.foodper(foodtotal));
-				System.out.printf("기념일에 쓴 돈의 비율 :%.2f\n ",diary.anniper(annitotal));
-				System.out.printf("남은 금액 : %.2f\n", diary.cmoney());
-				if((0.0<=diary.cmoney()) &&( diary.cmoney()<=400000*0.2)){
-					   System.out.println("!!!!! 경 고 !!!!! 아 껴 쓰 세 요 !!!!!!");
-				   }
-				if(diary.cmoney()<=0.0){
-					   System.out.println("적자났는데 더 쓸 거야????????");
-				   }
+				else if(k==2){
+					for(int j=0;j<count;j++){
+						   System.out.println("날짜 : "+date[j]+"\n"+"옷에 쓴 돈은 : "+cloth[j]+"\n"+"음식에 쓴 돈은 : "+food[j]+"\n"+"기념일에 쓴 돈은 : "+anniversary[j]+"\n");
+					}   
+					System.out.println("옷에 쓴 총 금액은:"+clothtotal+"\n"+"음식에 쓴 총 금액은:"+foodtotal+"\n"+"기념일에 쓴 총 금액은:"+annitotal+"\n");
+					System.out.println("총 금액 :"+diary.price());
+					System.out.printf("옷에 쓴 돈의 비율 :%.2f\n ",diary.clothper(clothtotal));
+					System.out.printf("음식에 쓴 돈의 비율 :%.2f\n ",diary.foodper(foodtotal));
+					System.out.printf("기념일에 쓴 돈의 비율 :%.2f\n ",diary.anniper(annitotal));
+					System.out.printf("남은 금액 : %.2f\n", diary.cmoney());
+					if((0.0<=diary.cmoney()) &&( diary.cmoney()<=400000*0.2)){
+						   System.out.println("!!!!! 경 고 !!!!! 아 껴 쓰 세 요 !!!!!!");
+					   }
+					if(diary.cmoney()<=0.0){
+						   System.out.println("적자났는데 더 쓸 거야????????");
+					   }
+				}
+				else if(k==3){
+					date[count]=0;
+					cloth[count]=0;
+					food[count]=0;
+					anniversary[count]=0;
+					balance=400000;
+				}
 			}
 	   }
  }
